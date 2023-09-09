c460bbe3c1bf2c53a41b437caeeb5a626d694719
Tristan Yang <yanggc888@163.com>
2023-09-09 09:21:10 +0800

    fix: status error

--- src/app/components/select-cellsize.tsx
+++ src/app/components/select-cellsize.tsx
@@ -14,7 +14,6 @@ const SelectZoom = () => {
   };
   return (
     <Select
-      disabled={status == "running"}
       value={cellSize}
       onChange={handleSizeChange}
       list={CellSizes.map((size) => {
