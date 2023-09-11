02928244bc272604778be3e7594bfedf2bc2986c
Tristan Yang <yanggc888@163.com>
2023-09-11 23:14:37 +0800

    feat: pb

--- src/app/components/window-records.tsx
+++ src/app/components/window-records.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import WindowTitleBar from "./window-title-bar";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import clsx from "clsx";
 import { updateMiniRecords } from "@/redux/slice/user.data";
 import Modal from "./modal";
+import Image from "next/image";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+import { PlayRecord } from "@/types";
 
 // type Props = {}
-
+dayjs.extend(relativeTime);
 const RecordsWindow = () => {
+  const [best, setBest] = useState<PlayRecord | null>(null);
   const dispatch = useAppDispatch();
   const recordWindowMinimized = useAppSelector((store) => store.userData.recordWindowMinimized);
   const records = useAppSelector((store) => store.userData.records);
+  useEffect(() => {
+    if (records.length > 0) {
+      const tmp = records.filter((r) => r.status == "win").sort((a, b) => a.duration - b.duration);
+      if (tmp.length > 0) {
+        setBest(tmp[0] as PlayRecord);
+      }
+    }
+  }, [records]);
+
   if (recordWindowMinimized) return null;
   return (
     <Modal mask={false}>
@@ -23,6 +38,18 @@ const RecordsWindow = () => {
           ></button>
         </WindowTitleBar>
         <div className="window-body">
+          {best && (
+            <div className="flex items-center gap-3 mb-4">
+              <Image width={32} height={32} alt="personal best" src={"/cert.png"} />
+              <div className="flex flex-col">
+                <span className="">Personal Best:</span>
+                <span className="text-sm">
+                  <strong>{best.level}</strong> level in <strong>{best.duration}s</strong> at{" "}
+                  <strong>{dayjs(best.timestamp).fromNow()}</strong>
+                </span>
+              </div>
+            </div>
+          )}
           <div className="sunken-panel w-fit min-h-[320px]">
             <table className="interactive w-full text-sm">
               <thead>
@@ -43,11 +70,7 @@ const RecordsWindow = () => {
                     const { duration, level, timestamp, status } = r;
                     return (
                       <tr key={timestamp} className="hover:bg-[#008] hover:text-white">
-                        <td>
-                          {`${new Date(timestamp).toLocaleDateString()} ${new Date(
-                            timestamp
-                          ).toLocaleTimeString()}`}
-                        </td>
+                        <td>{dayjs(timestamp).format("YYYY-MM-DD HH:mm:ss")}</td>
                         <td>{level}</td>
                         <td>{duration}s</td>
                         <td>{status}</td>
---
+++ src/app/guide/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// type Props = {};
+
+const GuidePage = () => {
+  return <div>GuidePage</div>;
+};
+
+export default GuidePage;
