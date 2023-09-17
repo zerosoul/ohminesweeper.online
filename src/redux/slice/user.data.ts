16623a49f9075e978d190338c0e0df498170fc62
Tristan Yang <yanggc888@163.com>
2023-09-17 17:04:09 +0800

    chore: updates

--- src/redux/slice/user.data.ts
+++ src/redux/slice/user.data.ts
@@ -4,7 +4,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Difficulty } from "@minesweeper";
 
 const initialState: UserData = {
-  article: "no-flag",
+  article: "",
   custom: defaultCustom,
   sound: false,
   minimized: false,
